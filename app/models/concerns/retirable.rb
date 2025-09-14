42c4bfa1d2e35cf7d8dcdfe0a626577d37750405
dambar08 <dambar1111@gmail.com>
2025-09-14 11:12:17 +0800

    add: validation for retirement consistency

--- app/models/concerns/retirable.rb
+++ app/models/concerns/retirable.rb
@@ -4,13 +4,24 @@ module Retirable
   included do
     validates :retired_by, presence: true, if: -> { retired_at.present? }
 
+    validate :retirement_consistency
+
     def retire(by:  Current.user, reason:)
       raise "Already retired" if retired_at.present?
-      update(retired_by: by, retired_at: Time.current, retire_reason: reason)
+      update!(retired_by: by, retired_at: Time.current, retire_reason: reason)
     end
 
     def retired?
       !!retired_at
+    end
+
+    private
+    def retirement_consistency
+      if retired_at.present? && retired_by.nil?
+        errors.add(:retired_by, "must be set if retired_at is present")
+      elsif retired_by.present? && retired_at.nil?
+        errors.add(:retired_at, "must be set if retired_by is present")
+      end
     end
   end
 end
